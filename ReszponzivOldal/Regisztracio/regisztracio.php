88a6c5f24debe2fcb914d25810234ef3c550dadd
Kakaogengszter <45355439+Kakaogengszter@users.noreply.github.com>
2019-03-01 10:58:32 +0100

    Add files via upload

--- ReszponzivOldal/Regisztracio/regisztracio.php
+++ ReszponzivOldal/Regisztracio/regisztracio.php
@@ -19,18 +19,16 @@ if (isset($_SESSION['uid'])){
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+  <script src="../jquery-3.3.1.min.js" charset="UTF-8"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="regisztracio.css">
 	<link rel="stylesheet" href="../reszponziv.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
 
-  <script src="../jquery-3.3.1.min.js" charset="UTF-8"></script>
+
   <script src="../validacio.js" charset="UTF-8"></script>
-<script>
-  $('.carousel').carousel();
-</script>
+
     <title>Regisztráció</title>
   </head>
   <body>
