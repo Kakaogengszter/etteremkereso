688e24fcb1b9116756b6a9d008cbea57dcc5c95c
Kakaogengszter <45355439+Kakaogengszter@users.noreply.github.com>
2019-03-12 13:50:22 +0100

    Add files via upload

--- ReszponzivOldal/menu.php
+++ ReszponzivOldal/menu.php
@@ -1,28 +1,6 @@
 
 
-<!DOCTYPE html>
-<html lang="hu">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <script src="../jquery-3.3.1.min.js" charset="UTF-8"></script>
 
-    <!-- Bootstrap CSS -->
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <link rel="stylesheet" href="../reszponziv.css">
-
-
-
-    <title>Szegedi Éttermek</title>
-
-
-  </head>
-  <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
 
   <a class="navbar-brand" href="#"></a>
