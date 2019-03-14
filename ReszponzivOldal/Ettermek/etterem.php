72bfb81c40fd78010b89070a18fbe62ee69991f7
Kakaogengszter <45355439+Kakaogengszter@users.noreply.github.com>
2019-03-14 10:33:36 +0100

    Add files via upload

--- ReszponzivOldal/Ettermek/etterem.php
+++ ReszponzivOldal/Ettermek/etterem.php
@@ -3,8 +3,17 @@ session_start();
 require'../menu.php';
 require_once('../connection.php');
 
+  if(isset($_GET['keresesertek']) AND !empty($_GET['keresesertek'])){
+    $keres = mysqli_real_escape_string($conn,$_GET['keresesertek']);
+    $sql = "SELECT * FROM etterem WHERE Nev LIKE '%$keres%' OR Tipus LIKE '%$keres%'";
 
-  $sql = "SELECT * FROM etterem";
+
+  }else
+  {
+
+      $sql = "SELECT * FROM etterem";
+  }
+
 
   $res = $conn -> query($sql);
 
@@ -77,11 +86,6 @@ $tabla.="</table>";*/
 
 
 
-  <nav>
-      <?php echo $menu; ?>
-  </nav>
-
-
 
 <div style="margin-top:100px;" class="d-flex flex-wrap align-items-center container-fluid col-md-10" >
   <?php
@@ -90,5 +94,8 @@ $tabla.="</table>";*/
 </div>
 
 
-  </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 </html>
